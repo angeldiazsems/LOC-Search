7c07d4cc5514de6747fdad3b7906acdd7f63772c
Francisco <fdiaz1231@outlook.com>
2024-01-18 17:16:49 -0800

    inital

---
+++ LOC-Search/src/main/java/LOC/Search/LOC/Search/LocSearchApplication.java
@@ -0,0 +1,13 @@
+package LOC.Search.LOC.Search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocSearchApplication.class, args);
+	}
+
+}
---
+++ LOC-Search/src/test/java/LOC/Search/LOC/Search/LocSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package LOC.Search.LOC.Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
