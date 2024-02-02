4390bbfdaff4c9430f15ac64510871f4bec3d92f
Francisco <fdiaz1231@outlook.com>
2024-02-01 21:21:32 -0800

    worked

---
+++ LOC-Search/src/main/java/LOCSearch/repository/dto/LocResponse.java
@@ -0,0 +1,20 @@
+package LOCSearch.repository.dto;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LocResponse {
+
+    private List<Result> results = null;
+
+    @JsonProperty("results")
+    public List<Result> getResults() {
+        return results;
+    }
+
+    @JsonProperty("results")
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+}
---
+++ LOC-Search/src/main/java/LOCSearch/repository/dto/Result.java
@@ -0,0 +1,40 @@
+package LOCSearch.repository.dto;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Result {
+
+    private String title;
+
+    @JsonAlias("contributor")
+    private List<String> authors;
+
+    @JsonAlias("id")
+    private String link;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+}
